189c54277f1ec3ea249b8b93127f5bcbf72627f4
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-09-25 16:31:54 +0530

    do-while

--- 24_Do-While-Loop-JS/script.js
+++ 24_Do-While-Loop-JS/script.js
@@ -1,1 +1,23 @@
 console.log("Do-While Loop");
+
+let i = 0
+
+console.log(`============do-while================`);
+do {
+    console.log(i);
+    i++
+} while (i<5);
+ 
+console.log(`============while================`);
+
+
+while (i<10) {
+    console.log(i);
+    i++
+}
+
+console.log(`============for================`);
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
