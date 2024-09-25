af26bd48cd96dc6c5828a7d264357f35c1f8eaff
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-09-25 12:11:50 +0530

    for loop

--- 23-For-Loop-JS/script.js
+++ 23-For-Loop-JS/script.js
@@ -1,7 +1,22 @@
+// debugger
 console.log('For Loop');
 
-for(let i = 0; i < 5; i++){
-    console.log(i + 1); 
+// for(let i = 0; i < 5; i++){
+//     console.log(i + 1); 
+// }
+// debugger
+const friends = ['sonic', 'viper', 'bheem', 'rise', 'wazir']
+
+// for (i = 0; i < friends.length; i++){
+//     console.log(friends[i]);
+//     friends[i] = friends[i] + 'is a professional'
+// }
+
+for (let i = 0; i <= 100; i++) {
+    if (i % 2 === 0) {
+    // if (i % 2 === 1) {
+        console.log(i);
+    } 
 }
 
 console.log("Program Ended");
