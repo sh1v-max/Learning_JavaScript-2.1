db16f1ae2a6e90ee9b8961d2d9c93faeecad52ea
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-09-28 15:13:13 +0530

    lexical and block scope

---
+++ 29_Lexical-And-Block-Scope-JS/script.js
@@ -0,0 +1,25 @@
+debugger
+const username = 'wazir'
+let userAge = 25
+var a = 5
+
+function add() {
+    const username = 'khan'
+    const x = 5
+    const y = 10
+    console.log(x + y)
+    console.log(username)
+}
+
+function sub() {
+    const x = 5
+    const y = 10
+    console.log(x - y)
+    console.log(username)
+}
+
+
+add()
+sub()
+
+console.log('Program Ended') 
