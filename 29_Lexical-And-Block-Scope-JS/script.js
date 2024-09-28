9c9ec231e2a6603ff7b7798dbdee14637c71a1b2
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-09-28 19:30:32 +0530

    lexical and block scope

--- 29_Lexical-And-Block-Scope-JS/script.js
+++ 29_Lexical-And-Block-Scope-JS/script.js
@@ -8,44 +8,44 @@ function add() {
   const y = 10               // Function scope
   console.log(x + y)
   console.log(username)
-}
 
-function sub() {
-  const x = 5                // Function scope
-  const y = 10               // Function scope
-  const z = 10               // Function scope
-  console.log(x - y)
-  console.log(username)
-
-  function child() {
-    const k = 12             // Function scope
-    const childName = 'khan' // Function scope
-    console.log(childName)
-    console.log(`My age is ${z}`)
-
-    {
-      const firstName = 'shiv'    // Block scope
-      const secondName = 'singh'  // Block scope
-      // var num2 = 100           // Global scope
-      // num3 = 200               // Global scope
-      const num = 101             // Block scope
-      console.log(`full name is ${firstName} ${secondName}`);
-      console.log(num)
-    }
-    // console.log(num2)
-    // console.log(num3)
-
-    function grandChild() {
-      const grandChildName = 'ritik' // Function scope
-      console.log(grandChildName)
+  function sub() {
+    const x = 5                // Function scope
+    const y = 10               // Function scope
+    const z = 10               // Function scope
+    console.log(x - y)
+    console.log(username)
+  
+    function child() {
+      const k = 12             // Function scope
+      const childName = 'khan' // Function scope
       console.log(childName)
-      console.log(username)
-      console.log(k)
+      console.log(`My age is ${z}`)
+  
+      {
+        const firstName = 'shiv'    // Block scope
+        const secondName = 'singh'  // Block scope
+        // var num2 = 100           // Global scope
+        // num3 = 200               // Global scope
+        const num = 101             // Block scope
+        console.log(`full name is ${firstName} ${secondName}`);
+        console.log(num)
+      }
+      // console.log(num2)
+      // console.log(num3)
+  
+      function grandChild() {
+        const grandChildName = 'ritik' // Function scope
+        console.log(grandChildName)
+        console.log(childName)
+        console.log(username)
+        console.log(k)
+      }
+      grandChild()
     }
-    grandChild()
+    child()
   }
-  child()
+  sub()
 }
-
-sub()
+add()
 console.log('Program Ended')
