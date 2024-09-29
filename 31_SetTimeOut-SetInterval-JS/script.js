ccbaa3747ca0e243c56f2ea671672273edfed3a8
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-09-29 12:54:47 +0530

    settimeout setinterval

--- 31_SetTimeOut-SetInterval-JS/script.js
+++ 31_SetTimeOut-SetInterval-JS/script.js
@@ -5,6 +5,7 @@
 //     console.log("hello wazir")
 //     console.log("i am wazir")`, 500);
 
+/*
 const timer1 = setTimeout(`console.log("wazir1")`, 500);
 const timer2 = setTimeout(`console.log("wazir2")`, 1000);
 const timer3 = setTimeout(a, 1500);
@@ -17,3 +18,38 @@ function a() {
 }
 
 const timer4 = setTimeout(console.log("wazirx"), 1000);
+*/
+
+
+/*
+const timer1 = setInterval(`console.log("wazir1")`, 500);
+const timer2 = setInterval(`console.log("wazir2")`, 1000);
+const timer3 = setInterval(a, 1500);
+
+// clearInterval(timer2);
+
+function a() {
+    console.log("hello wazir");
+    return `hello`
+}
+
+const timer4 = setInterval(console.log("wazirx"), 1000);
+*/
+
+
+/*
+setTimeout(function() {
+    console.log("hello wazir-1");  //asynchronous code
+}, 0);
+
+
+console.log("hello wazir-2");    //synchronous code
+*/
+
+//setTimeout with anonymous function
+setTimeout(function() {        //anonymous function
+    console.log("hello wazir-1");
+}, 2000);
+
+
+console.log("hello wazir-2");
