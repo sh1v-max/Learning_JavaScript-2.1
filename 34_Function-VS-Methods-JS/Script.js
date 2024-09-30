181e35b6c5e08326a8ed072794aa73e570665b01
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-09-30 20:09:06 +0530

    functions vs methods

--- 34_Function-VS-Methods-JS/Script.js
+++ 34_Function-VS-Methods-JS/Script.js
@@ -1,3 +1,16 @@
-function add(a, b) {
-  return a + b
+const maths = {
+  E: 2.718281828459045,
+  add: function (a, b) {
+    return a + b
+  },
+  sub: function (a, b) {
+    return a - b
+  },
+  pow(a, b) {           // This is a shorthand method
+    return a ** b
+  },
+  cube(a) {           // This is a shorthand method
+    return a ** 3
+  }
 }
+
