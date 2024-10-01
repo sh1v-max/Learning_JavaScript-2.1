e250c33335a6b0b93ef00c82601e9ce4eca137f1
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-10-01 18:28:01 +0530

    forEach

--- 37_ForEach-Array-method-JS/script.js
+++ 37_ForEach-Array-method-JS/script.js
@@ -1,5 +1,67 @@
-const fruits = ['Apple', 'Banana', 'Orange', 'Mango', 'Pineapple'];
+const fruits = ['Apple', 'Banana', 'Orange', 'Mango', 'Pineapple']
 
 // for(const fruit of fruits){
 //     console.log(fruit);
 // }
+
+//its a method and it takes a function(callback) as an argument
+fruits.forEach(function () {
+  // console.log('I am a fruit')
+  // it iterate and print 'I am a fruit' 5 times(number of elements in the array)
+})
+
+//with regular function
+fruits.forEach(function (fruit) {
+  console.log(fruit)
+})
+
+//with arrow function
+//and its a great example of a higher order function
+fruits.forEach((fruit) => {
+  // .forEach() is a higher order function
+  console.log(fruit) // arrow function(anonymous) is a callback function
+})
+
+/*
+fruits.forEach(abc)
+function abc(el){
+  console.log(el)
+}
+        
+OR
+
+fruits.forEach(function abc(el){                 //named function
+  console.log(el)
+})
+*/
+
+/*
+return type of forEach is undefined
+
+fruits.forEach((fruit) => {
+  console.log(fruit)
+  return fruit
+})
+  output:
+      Apple
+      Banana
+      Orange
+      Mango
+      Pineapple
+      undefined
+
+? quick peak of MAP method 
+using map wont change the original array
+it also returns an array
+fruits.map((fruit) => {
+  console.log(fruit)
+  return fruit
+})
+  output:
+  Apple
+  Banana
+  Orange
+  Mango
+  Pineapple
+(5) ['Apple', 'Banana', 'Orange', 'Mango', 'Pineapple']
+*/
