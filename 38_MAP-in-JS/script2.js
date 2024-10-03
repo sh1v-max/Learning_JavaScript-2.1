44e650b1efc4c00b278744052669a6ed873a4095
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-10-03 12:05:29 +0530

    map filter and reduce

--- 38_MAP-in-JS/script2.js
+++ 38_MAP-in-JS/script2.js
@@ -20,8 +20,32 @@ const exReduced = exArr.reduce((accumulator, currentValue, index, array) => {
 const nums = [1, 2, 3, 4]
 const sum = nums.reduce((accumulator, currentValue, index, array) => {
   // console.log(index, currentValue) 
-  // console.log(accumulator, currentValue) 
-  // debugger
-  return accumulator+currentValue
-},1) // 
-// console.log(sum)
+  console.log(accumulator, currentValue)  // 1 2 3 4
+  // debugger //
+  return accumulator+currentValue // 1+2+3+4
+},0) // 
+ console.log(sum)
+
+
+const usersCamp = [
+  {firstName: 'John', lastName: 'Doe', age: 30},
+  {firstName: 'akshay', lastName: 'singh', age: 22},
+  {firstName: 'wazir', lastName: 'roy', age: 28},
+  {firstName: 'ritik', lastName: 'ob', age: 34},
+]
+const user30 = usersCamp.reduce((acc ,curr) => {
+  if(curr.age < 30){ //if the age is less than 30
+    acc = acc.concat(curr.firstName) //concatenate the first name
+  }
+  return acc
+}, [])
+
+console.log(user30)
+// const user30 = usersCamp.filter((user) => {
+//   return user.age < 30
+// })
+// console.log(user30)
+// const user30Name = user30.map((user) => {
+//   return user.firstName
+// })
+// console.log(user30Name)
