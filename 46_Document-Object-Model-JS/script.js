fcd74d7ebee0d6fbb73f448ed828f2f22e241232
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-10-08 13:43:42 +0530

    dom in js

--- 46_Document-Object-Model-JS/script.js
+++ 46_Document-Object-Model-JS/script.js
@@ -6,3 +6,4 @@ function sayHello() {
 
 // document.body.children[4].src = 'https://images5.alphacoders.com/137/1372790.png'
 // the script tag must be at the end of the body tag to work
+//! its called DOM manipulation 
