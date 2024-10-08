9ba24fd4083fe7dfb612b0851d6e0fe3d8202b93
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-10-08 13:36:51 +0530

    dom in js

---
+++ 46_Document-Object-Model-JS/script.js
@@ -0,0 +1,8 @@
+function sayHello() {
+  document.body.children[4].src = 'https://images5.alphacoders.com/137/1372790.png'
+  // the script tag must be at the end of the body tag to work
+}
+//calling the function will change the image
+
+// document.body.children[4].src = 'https://images5.alphacoders.com/137/1372790.png'
+// the script tag must be at the end of the body tag to work
