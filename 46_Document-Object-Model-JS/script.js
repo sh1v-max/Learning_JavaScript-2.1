cbb2c367091f3cb43987ad8112dace7d54b55620
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-10-08 23:38:05 +0530

    dom

--- 46_Document-Object-Model-JS/script.js
+++ 46_Document-Object-Model-JS/script.js
@@ -1,4 +1,4 @@
-function sayHello() {
+function changeImage1() {
   document.body.children[4].src = 'https://images5.alphacoders.com/137/1372790.png'
   // the script tag must be at the end of the body tag to work
 }
@@ -6,4 +6,12 @@ function sayHello() {
 
 // document.body.children[4].src = 'https://images5.alphacoders.com/137/1372790.png'
 // the script tag must be at the end of the body tag to work
-//! its called DOM manipulation z
+//! its called DOM manipulation
+
+function changeImage2() {
+  document.body.children[5].children[2].children[4].src = 'https://pharmawiki.in/wp-content/uploads/2012/04/images5.jpg'
+}
+
+function changeImage3() {
+  document.body.children[5].children[3].children[4].src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeYWDxjt9GrMZ2-5FD4fFtSFM0n0bbSMCXwg&s'
+}
