8cc59a586e73cfb98cb4522dfa7cf0631579e756
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-10-14 19:13:32 +0530

    updated

--- 48_InnerText-InnerHTML-TextContent/script.js
+++ 48_InnerText-InnerHTML-TextContent/script.js
@@ -1,5 +1,14 @@
 const paragraph = document.querySelector('p');
 console.log(paragraph.innerHTML)
-//output 
-// paragraph.innerHTML = '<h4>Hiii</h4>'
-paragraph.innerHTML
+
+// innerText - returns the text content of the element which is visible on the screen
+const innerText = paragraph.innerText;
+console.log(innerText);
+
+// innerHTML - returns the HTML content of the element including the tags
+const innerHTML = paragraph.innerHTML;
+console.log(innerHTML);
+
+// textContent - returns the text content of the element
+const textContent = paragraph.textContent;
+console.log(textContent);
