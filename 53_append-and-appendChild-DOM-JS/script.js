340ea1289f8caf9688b8487e427a98ef2256d183
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-10-16 11:55:08 +0530

    append and appendChild

---
+++ 53_append-and-appendChild-DOM-JS/script.js
@@ -0,0 +1,6 @@
+//! always use "defer" in script tag in html file to run the script after the html is loaded
+//! or use "DOMContentLoaded" event listener to run the script after the html is loaded
+//! or put the script tag at the end of the body tag
+
+const container = document.querySelector(".container")
+const card = document.querySelector('.card')
