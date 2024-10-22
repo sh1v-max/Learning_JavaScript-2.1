517f9f4c298cbbecfa44f090d18fe6adb761168b
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-10-22 09:46:19 +0530

    form event

--- 57_form-event-and-event-object-DOM-JS/scrypt.js
+++ 57_form-event-and-event-object-DOM-JS/scrypt.js
@@ -2,6 +2,22 @@
 
 const usernameInput = document.querySelector('#username')
 
-usernameInput.addEventListener('dblclick', () => {
-  console.log('input clicked')
+// usernameInput.addEventListener('click', () => {
+//   console.log('input clicked')
+// }) 
+
+// usernameInput.addEventListener('dblclick', () => {
+//   console.log('input double clicked')
+// }) 
+usernameInput.addEventListener('input', () => {
+  // console.log('some input')
+}) 
+// input event is Triggered every time the user types in a text input or textarea
+usernameInput.addEventListener('input', (event) => {
+  console.log(event)
+  // returns the event object
+  // InputEvent {isTrusted: true, data: 'a', isComposing: false, inputType: 'insertText', dataTransfer: null, …}
+  console.log(event.target.value)
+  // returns the value of the input entered
+  console.log('some input')
 }) 
