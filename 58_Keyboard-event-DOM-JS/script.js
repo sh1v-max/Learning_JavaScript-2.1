8fc5672e98817dd287ece624fd5c69ed9ee2c2c3
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-10-23 13:50:55 +0530

    keyboard events

---
+++ 58_Keyboard-event-DOM-JS/script.js
@@ -0,0 +1,10 @@
+const h1 = document.querySelector('h1')
+const input = document.querySelector('input')
+
+h1.addEventListener('keypress', (e) => {
+  console.log(e)
+})
+
+input.addEventListener('keypress', (e) => {
+  console.log(e)
+})
