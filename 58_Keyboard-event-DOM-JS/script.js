a032a60a376c2813759576005a4afc0a1719f936
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-10-24 10:16:24 +0530

    keyboard events

--- 58_Keyboard-event-DOM-JS/script.js
+++ 58_Keyboard-event-DOM-JS/script.js
@@ -1,10 +1,35 @@
 const h1 = document.querySelector('h1')
 const input = document.querySelector('input')
 
+/*
 h1.addEventListener('keypress', (e) => {
-  console.log(e)
+  // console.log(e)
+  console.log(e.key) // log the key that was pressed
 })
 
 input.addEventListener('keypress', (e) => {
   console.log(e.key)
 })
+*/
+
+window.addEventListener('keypress', (e) => {
+  // fired when key is presses
+  console.log('Code', e.code) // log the code of the key that was pressed
+  console.log('Value', e.key) // log the value that was pressed 
+}) 
+// we usually apply these events on window object, so that we can listen to the keypress event on the whole window
+
+window.addEventListener('keyup', (e) => {
+  // fired when key is released
+  console.log('Code', e.code) // log the code of the key that was pressed
+  console.log('Value', e.key) // log the value that was pressed 
+}) 
+
+window.addEventListener('keydown', (e) => {
+  // fired until key is released
+  console.log('Code', e.code) // log the code of the key that was pressed
+  console.log('Value', e.key) // log the value that was pressed 
+}) 
+
+// keypress wont work on shift, ctrl, alt, etc.
+// keydown and keyup will work on shift, ctrl, alt, etc.
