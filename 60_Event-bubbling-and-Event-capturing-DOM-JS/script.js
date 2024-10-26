7621cafe031cce4096d0c645a5603a5283133980
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-10-26 17:38:20 +0530

    bubbling and capturing

--- 60_Event-bubbling-and-Event-capturing-DOM-JS/script.js
+++ 60_Event-bubbling-and-Event-capturing-DOM-JS/script.js
@@ -3,23 +3,35 @@ const pink = document.querySelector('.pink')
 const blue = document.querySelector('.blue')
 
 //^ bubbling is the default behavior of the event propagation in the DOM. It means that when an event is fired on an element, it will bubble up to the parent element.
-window.addEventListener('click', () => {
-    console.log('window event listened')
-})
-document.addEventListener('click', () => {
-    console.log('document event listened')
-})
-document.body.addEventListener('click', () => {
-    console.log('body event listened')
-})
-green.addEventListener('click', () => {
-    console.log('green event listened')
-})
-pink.addEventListener('click', () => {
-    console.log('pink event listened')
-})
-blue.addEventListener('click', () => {
-    console.log('blue event listened')
-})
+window.addEventListener('click', (e) => {
+    console.log('6. window event listened')
+}, true)
+document.addEventListener('click', (e) => {
+    console.log('5. document event listened')
+}, true)
+document.body.addEventListener('click', (e) => {
+    console.log('4. body event listened')
+}, true)
+green.addEventListener('click', (e) => {
+  e.stopPropagation() 
+  // stopPropagation() method stops the bubbling of an event to parent elements, preventing further bubbling.
+    console.log('3. green event listened')
+}, true)
+pink.addEventListener('click', (e) => {
+    console.log('2. pink event listened')
+  }, {capture: true})
+// we can also use the object to set the event to be captured, not bubbled.
+// {capture: true} is used to set the event to be captured, not bubbled.
 
+blue.addEventListener('click', (e) => {
+  // e.stopPropagation() 
+  // stopPropagation() method stops the bubbling of an event to parent elements, preventing further bubbling.
+    console.log('1. blue event listened')
+}, {capture: true, once: true})
+//{once: true} is used to set the event to be executed only once.
+
+// true in addEventListener() method is used to set the event to be captured, not bubbled.
+//it will print the output in the order of the event capturing.
+// ie. window, document, body, green, pink, blue and will stop when encountered stopPropagation() method.
 //~ if we click on blue, it will bubble up to the parent element, pink, and then green, and then body, document, and window.
+
