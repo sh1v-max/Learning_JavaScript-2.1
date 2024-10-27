4fd258c2bcdcaee360c4bb3fc5379f4769847640
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-10-27 12:08:28 +0530

    focus and blur

--- 61_Event-Simulator-DOM-JS/script.js
+++ 61_Event-Simulator-DOM-JS/script.js
@@ -1,5 +1,6 @@
 const addCardBtn = document.querySelector('.card')
 const container = document.querySelector('.container')
+const input = document.querySelector('input')
 
 let count = 1
 
@@ -10,6 +11,7 @@ addCardBtn.addEventListener('click', () => {
     container.append(newCard)
 })
 
+// what if we want to click the addCardBtn 1000 times?
 //^ click event
 //~ adding 1000 cards
 // for(let i = 0; i < 1000; i++) {
@@ -18,9 +20,27 @@ addCardBtn.addEventListener('click', () => {
 
 //~ adding 1000 cards with setInterval
 // and to stop the setInterval, we can use clearInterval using the return value of setInterval(id)
-const intervalId = setInterval( () => {
-    if(count > 999){
-        clearInterval(intervalId)
-    }
-    addCardBtn.click() //^ click event on addCardBtn
-}, 1)
+// const intervalId = setInterval( () => {
+//     if(count > 999){
+//         clearInterval(intervalId)
+//     }
+//     addCardBtn.click() //^ click event on addCardBtn
+// }, 1)
+
+
+//^ focus event
+setTimeout(() => {
+    input.focus()
+    console.log('input focused')
+}, 1000);
+// input.focus()
+
+//^ blur event
+setTimeout(() => {
+    input.blur()
+    console.log('input blurred')
+}, 3000);
+// input.blur()
+
+// this basically focuses and blurs the input field after 1 and 3 seconds respectively
+// that means it simulate clicking on the input field and then clicking outside of the input field in interval of 1 and 3 seconds respectively 
