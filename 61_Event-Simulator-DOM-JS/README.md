f3268bc0f01599e6dac2cd58562ead742167ab05
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-10-27 10:13:35 +0530

    click simulator

--- 61_Event-Simulator-DOM-JS/README.md
+++ 61_Event-Simulator-DOM-JS/README.md
@@ -2,5 +2,5 @@
 To know more about Mouse events in DOM JS, visit:
 
 > - [freeCodeCamp](https://www.freecodecamp.org/news/event-bubbling-and-event-capturing-in-javascript/)
-> - [YouTube (Recommended)](https://www.youtube.com/watch?v=lfRgu5dLh8E&list=PLfEr2kn3s-br9ZFmejfLhAgMbGgbpdof8&index=106)
+> - [YouTube (Recommended)](https://www.youtube.com/watch?v=uKupoqAtJBk&list=PLfEr2kn3s-br9ZFmejfLhAgMbGgbpdof8&index=107)
 
--- 61_Event-Simulator-DOM-JS/script.js
+++ 61_Event-Simulator-DOM-JS/script.js
@@ -9,3 +9,18 @@ addCardBtn.addEventListener('click', () => {
     newCard.innerText = count++
     container.append(newCard)
 })
+
+//^ click event
+//~ adding 1000 cards
+// for(let i = 0; i < 1000; i++) {
+//     addCardBtn.click()
+// }
+
+//~ adding 1000 cards with setInterval
+// and to stop the setInterval, we can use clearInterval using the return value of setInterval(id)
+const intervalId = setInterval( () => {
+    if(count > 999){
+        clearInterval(intervalId)
+    }
+    addCardBtn.click() //^ click event on addCardBtn
+}, 1)
