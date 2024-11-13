97373d3effa958c89a72d1285fff65f7a0c3f1a9
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-11-13 16:31:33 +0530

    json stringfy

--- 63_Local-Storage-JS/script.js
+++ 63_Local-Storage-JS/script.js
@@ -2,16 +2,45 @@
 
 const nameElement = document.querySelector('.name-tag')
 const nameInput = document.querySelector('.name')
+const ageElement = document.querySelector('.age-tag')
+const ageInput = document.querySelector('.age')
 
 // nameElement.innerText = localStorage.myName
-nameElement.innerText = localStorage.getItem('myName')
-//~ this is the correct way of using local storage
+// nameElement.innerText = localStorage.getItem('myName')
+// //~ this is the correct way of using local storage
+
+// nameInput.addEventListener('input', (e) => {
+//   // localStorage.myName = e.target.value
+//   localStorage.setItem('myName', e.target.value) 
+//   //~ note this step
+//   nameElement.innerText = localStorage.getItem('myName')
+// })
+
+// //! but updating value using this method is not that good
+// // minor difference between the two methods
+// // localStorage.getItem('age')
+// // will return "null"
+// // localStorage.age
+// // will return "undefined"
+
+// ageElement.innerText = localStorage.getItem('myAge')
+// ageInput.addEventListener('input', (e) => {
+//   localStorage.setItem('myAge', e.target.value);
+//   ageElement.innerText = localStorage.getItem('myAge')
+// })
+
+//^ i want to store both at a time, so i am using object
+const myData = {
+  name: '',
+  age: '',
+}
 
 nameInput.addEventListener('input', (e) => {
-  // localStorage.myName = e.target.value
-  localStorage.setItem('myName', e.target.value) 
-  //~ note this step
-  nameElement.innerText = localStorage.myName
+  myData.name = e.target.value
+  localStorage.setItem('myData', JSON.stringify(myData))
 })
 
-//! but updating value using this method is not that good
+ageInput.addEventListener('input', (e) => {
+  myData.age = e.target.value
+  localStorage.setItem('myData', JSON.stringify(myData))
+})
