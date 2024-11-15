fe43449916a1e23b8cda1683bf46eefdb51bf8db
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-11-15 11:41:33 +0530

    readme update

--- 64_API-talking-to-the-outside-world-JS/script.js
+++ 64_API-talking-to-the-outside-world-JS/script.js
@@ -1,6 +1,8 @@
 const user = {
-  "userId": 1,
-  "id": 1,
-  "title": "delectus aut autem",
-  "completed": false
+  userId: 1,
+  id: 1,
+  title: 'delectus aut autem',
+  completed: false,
 }
+
+//JSON is a string representation of an object in JavaScript 
