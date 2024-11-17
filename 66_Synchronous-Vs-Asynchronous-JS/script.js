d1a5c9f2e7e376c90f52e2a2d52bdfab6691c9b7
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-11-17 17:05:46 +0530

    synchronous

--- 66_Synchronous-Vs-Asynchronous-JS/script.js
+++ 66_Synchronous-Vs-Asynchronous-JS/script.js
@@ -1,25 +1,40 @@
 const image = document.querySelector('img')
 const button = document.querySelector('button')
+const blockingBtn = document.querySelector('.blocking-btn')
 
-// button.addEventListener('click', () => {
-//   const xhr = new XMLHttpRequest()
+button.addEventListener('click', () => {
+  const xhr = new XMLHttpRequest()
 
-//   // xhr.responseType = 'json'
+  // xhr.responseType = 'json'
 
-//   xhr.addEventListener('load', () => {
-//     image.src = JSON.parse(xhr.response).message
-//     console.log(xhr);
-//   })
+  xhr.addEventListener('load', () => {
+    image.src = JSON.parse(xhr.response).message
+    console.log(xhr)
+  })
 
-//   // xhr.onload = () => {
-//   //   image.src = xhr.response.message
-//   //   console.log(xhr)
-//   // }
+  // xhr.onload = () => {
+  //   image.src = xhr.response.message
+  //   console.log(xhr)
+  // }
 
-//   xhr.open('GET', 'https://dog.ceo/api/breeds/image/random', false)
-//   xhr.send()
-// })
+  xhr.open('GET', 'https://dog.ceo/api/breeds/image/random', false)
+  xhr.send()
+})
 
-setTimeout(() => {
-  console.log('hii');
-}, 2000);
+// ^ setTimout is a asynchronous function
+// setTimeout(() => {
+//   console.log('hii');
+// }, 4000);
+
+// ^ alert is a synchronous function
+// alert('hello')
+
+//^ another example of synchronous function 
+blockingBtn.addEventListener('click', () => {
+  const startTime = Date.now()
+  let currentTime = startTime
+
+  while (startTime + 5000 > currentTime) {
+    currentTime = Date.now()
+  }
+})
