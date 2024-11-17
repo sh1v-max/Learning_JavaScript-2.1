7fcfd88d7aa8d5f30903b411ac3c2ea1b842b99f
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-11-17 13:26:57 +0530

    setTimeout

--- 66_Synchronous-Vs-Asynchronous-JS/script.js
+++ 66_Synchronous-Vs-Asynchronous-JS/script.js
@@ -1,29 +1,25 @@
 const image = document.querySelector('img')
 const button = document.querySelector('button')
 
-button.addEventListener('click', () => {
-  // debugger;
-  const xhr = new XMLHttpRequest()
-  xhr.responseType = 'json'
-  
-  //^ using eventlistener with load property
-  // xhr.addEventListener('load', () => { // load event is fired when the request has successfully completed
-    // console.log(xhr.response) // its in json format
-    // console.log(JSON.parse(xhr.response)) // converting json to js object
-    // image.src = xhr.response.message
-    // console.log(xhr)
-    // console.log('successfully loaded')
-    // setting the src of the image to the url of the image
-  // })
-  
-  //^ using onload property
-  xhr.onload = () => {
-    // debugger
-    image.src = xhr.response.message
-    console.log(xhr)
-    console.log('successfully loaded')
-  }
-  
-  xhr.open('GET', 'https://dog.ceo/api/breeds/image/random')
-  xhr.send()
-})
+// button.addEventListener('click', () => {
+//   const xhr = new XMLHttpRequest()
+
+//   // xhr.responseType = 'json'
+
+//   xhr.addEventListener('load', () => {
+//     image.src = JSON.parse(xhr.response).message
+//     console.log(xhr);
+//   })
+
+//   // xhr.onload = () => {
+//   //   image.src = xhr.response.message
+//   //   console.log(xhr)
+//   // }
+
+//   xhr.open('GET', 'https://dog.ceo/api/breeds/image/random', false)
+//   xhr.send()
+// })
+
+setTimeout(() => {
+  console.log('hii');
+}, 2000);
