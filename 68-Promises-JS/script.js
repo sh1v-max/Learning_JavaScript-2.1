48a7acf4a06b9f5616ace40205e5d436a4727a83
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-11-19 14:09:17 +0530

    understanding promises

--- 68-Promises-JS/script.js
+++ 68-Promises-JS/script.js
@@ -1,2 +1,15 @@
 //^ promise is a object
 // that may produce a single value some time in the future
+// its a constructor function, so new keyword is needed to crease a promise
+
+//~ creating a promise
+
+const btn = document.querySelector('#resolve-btn')
+
+const p = new Promise ((resolve, reject) => {
+  // resolve('wazir')
+  // reject({name: 'wazir'})
+  setTimeout(() => {
+    resolve({name: 'Wazir'})
+  }, 4000);
+})
