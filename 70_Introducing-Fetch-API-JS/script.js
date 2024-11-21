100e985b4a25354cdc60ad6a60efde9d260037f7
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-11-21 20:08:36 +0530

    understanding fetch

--- 70_Introducing-Fetch-API-JS/script.js
+++ 70_Introducing-Fetch-API-JS/script.js
@@ -1,4 +1,44 @@
+//~ fetch('https://dummyjson.com/products').then((res) => {
+  // console.log(res)
+  // res.json()
+  // console.log(res.json())// this returns a promise, we can apply .then
+  // res.json().then((data) => {
+  //   console.log(data) // this will return the actual data
+  // }) 
+  //as res.json() itself is a promise, we can again apply .then
+//~   res.json().then((data) => {
+//~     console.log(data)
+//~   })
+//~ })
+
+//^ whenever we use fetch to send request, we dont get the data directly, 
+//^ we get a response object, which we can use to get the data
+// in the above code, we are using res.json() to extract the data from the response
+// console.log(res.json() its a promise
+
+//& fetch('https://dummyjson.com/products').then((res) => {
+//   console.log(res)
+// })
+//output: Response {type: 'cors', url: 'https://dummyjson.com/products', 
+//        redirected: false, status: 200, ok: true, …}
+//  returns a Promise that resolves to a Response object
+
+//& fetch('https://dummyjson.com/products').then((res) => {
+//   console.log(res.json())
+// })
+//output: Promise {<pending>}
+// here res.json() converts the Response object (res) to a JavaScript object by 
+// parsing the JSON content in the response body
+
+//^ fetch('https://dummyjson.com/products').then((res) => {
+// res.json().then((data) => {
+//     console.log(data)
+//   })
+// })
+// above code can be written as below
+
 fetch('https://dummyjson.com/products').then((res) => {
-  console.log(res.json())// this returns a promise
-  console.log(res)
+  return res.json()// as it return a promise, we can apply .then
+}).then((data) => {
+  console.log(data)
 })
