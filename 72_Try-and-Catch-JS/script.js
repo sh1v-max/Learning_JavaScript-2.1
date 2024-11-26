5c39244f8ae974fb03c1854205d7d7eb1ee423c2
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-11-26 14:31:28 +0530

    try catch with async await

--- 72_Try-and-Catch-JS/script.js
+++ 72_Try-and-Catch-JS/script.js
@@ -8,7 +8,7 @@
 //~ try{
 //   console.log(a)
 // }catch(err){
-//   console.dir(err) // output: ReferenceError: a is not defined at script.js:9:15 
+//   console.dir(err) // output: ReferenceError: a is not defined at script.js:9:15
 //   //! this "err" is an object and not a string
 // }
 
@@ -17,19 +17,41 @@
 
 const user = {
   name: 'Wazir',
-  age: 34
+  age: 34,
 }
 // console.log(user.address) // output: undefined
-// console.log(user.address.city) 
+// console.log(user.address.city)
 // output: Uncaught TypeError: Cannot read properties of undefined (reading 'city')
 // at script.js:23:26
 
-try{
-  // console.log(user.address) // output: undefined
-  console.log(user.address.city) 
-}catch(err){
-  console.dir(err) 
-  // error will be logged in the console
-  console.dir(err.name) // return the type of the error (reference error/type error etc)
-  console.dir(err.message) // return error message (cannot read property of undefined)
+//~ try{ //^ will run always
+// console.log(user.address) // output: undefined
+//   console.log(user.address.city)
+// }catch(err){ //^ will run only if there is an error
+//   console.dir(err)
+// error will be logged in the console
+//   console.dir(err.name) // return the type of the error (reference error/type error etc)
+//   console.dir(err.message) // return error message (cannot read property of undefined)
+// }finally{ //^ wil run always
+//   console.log('finally block') // this block will always run
+// }
+
+// console.log('hello') // output: hello
+// console.log(3 + 7) // output: 10
+
+//^ try/catch with async/await
+async function makeAsyncRequest() {
+  try {
+    const url = 'https://dummyjson.com/products'
+    const response = await fetch(url)
+    const data = await response.json()
+    console.log(data)
+    return data
+  } catch (err) {
+    console.dir(err)
+  }
+
+  console.log('hello')
 }
+
+makeAsyncRequest()
