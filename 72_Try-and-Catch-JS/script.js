7b6597eb140baaa7b1529b426b8e3d9f61fdab04
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-11-26 13:51:21 +0530

    try/cath with object

--- 72_Try-and-Catch-JS/script.js
+++ 72_Try-and-Catch-JS/script.js
@@ -5,14 +5,28 @@
 
 //^ to encounter the error and still print "hello" we use try and catch block
 
-try{
-  console.log(a)
-}catch(err){
-  console.dir(err) // output: ReferenceError: a is not defined at script.js:9:15 
-  //! this "err" is an object and not a string
+//~ try{
+//   console.log(a)
+// }catch(err){
+//   console.dir(err) // output: ReferenceError: a is not defined at script.js:9:15 
+//   //! this "err" is an object and not a string
+// }
+
+// console.log('hello') // output: hello
+// console.log(3 + 7) // output: 10
+
+const user = {
+  name: 'Wazir',
+  age: 34
 }
+// console.log(user.address) // output: undefined
+// console.log(user.address.city) 
+// output: Uncaught TypeError: Cannot read properties of undefined (reading 'city')
+// at script.js:23:26
 
-console.log('hello') // output: hello
-console.log(3 + 7) // output: 10
-
-console.log("hello")
+try{
+  // console.log(user.address) // output: undefined
+  console.log(user.address.city) 
+}catch(err){
+  console.dir(err) 
+}
