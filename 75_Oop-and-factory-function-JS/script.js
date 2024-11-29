8956b76739691f0f93b6d6f5c6a8b88e1994f760
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-11-29 17:32:49 +0530

    readme

--- 75_Oop-and-factory-function-JS/script.js
+++ 75_Oop-and-factory-function-JS/script.js
@@ -1,5 +1,16 @@
+// object oriented programming is a programming paradigm based 
+// on the concept of objects which can contain data in the form of 
+// fields and code in the form of procedures. An object can be 
+// defined as a data field that has unique attributes and behavior. 
+// The object-oriented programming is a way to solve the problem by 
+// creating objects.
+
 const user = {
   firstName: 'Anurag',
   lastName: 'Singh',
-  age: 25
+  age: 25,
+  getAgeYear: function() {
+    return new Date().getFullYear() - age
+  }
 }
+
