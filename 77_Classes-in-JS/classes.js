2583332b7241f7ee8de89cdcb2367e49903584b4
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-12-05 20:10:12 +0530

    class logic

--- 77_Classes-in-JS/classes.js
+++ 77_Classes-in-JS/classes.js
@@ -33,17 +33,27 @@ function userExample(){}
 //^ we can write the prototype methods inside the class
 
 class CreateUser {
+  constructor(a, b, c){
+    console.log(this)
+    console.log(a, b, c)
+    // class with print this, as being called using new keyword
+  }
+  // console.log("hello") // it wont get printer, and will return
   getBirthYear() {
     return new Date().getFullYear() - this.age
+    // console.log("hello") // it wont get printer either, and will return
   }
 
   getFullName() {
     return this.firstName + ' ' + this.lastName
   }
+  //if we want to log something, we must log inside constructor function and not directly inside class
 }
+//we can save the methods inside the prototype object like this also
 
 
 // it will add two properties in prototype object
 
 const user1 = new CreateUser('Aman', 'Mishra', 32)
 const user2 = new CreateUser('Anurag', 'Singh', 72)
+// this automatically goes to constructor function
