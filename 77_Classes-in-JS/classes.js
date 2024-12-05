bac869e21d565dc8567a6b4aab2513f1df0c22ca
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-12-05 19:58:51 +0530

    class

--- 77_Classes-in-JS/classes.js
+++ 77_Classes-in-JS/classes.js
@@ -17,16 +17,33 @@ function userExample(){}
 // new CreateUser() will return an object with the properties defined in the constructor
 // output: CreateUser {}
 
+//~ class CreateUser {
+
+// }
+
+// CreateUser.prototype.getBirthYear = function() {
+//   return new Date().getFullYear() - this.age
+// }
+
+// CreateUser.prototype.getFullName = function() {
+//   return this.firstName + ' ' + this.lastName
+// }
+
+//^ we can also write the above code in class as below
+//^ we can write the prototype methods inside the class
+
 class CreateUser {
+  getBirthYear() {
+    return new Date().getFullYear() - this.age
+  }
 
+  getFullName() {
+    return this.firstName + ' ' + this.lastName
+  }
 }
 
-CreateUser.prototype.getBirthYear = function() {
-  return new Date().getFullYear() - this.age
-}
-
-CreateUser.prototype.getFullName = function() {
-  return this.firstName + ' ' + this.lastName
-}
 
 // it will add two properties in prototype object
+
+const user1 = new CreateUser('Aman', 'Mishra', 32)
+const user2 = new CreateUser('Anurag', 'Singh', 72)
