81d5f9f62d36e6e14c59e36be04cc1bcb1696b0f
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-12-07 20:13:18 +0530

    private

--- 78_How-to-make-private-class-properties-JS/classes.js
+++ 78_How-to-make-private-class-properties-JS/classes.js
@@ -1,16 +1,23 @@
 class CreateUser {
+  #age
+  // #age // this is a private property
   constructor(firstName, lastName, age) {
     this.firstName = firstName
     this.lastName = lastName
-    this._age = age 
+    // this._age = age 
     // using underscore was a convention to tell other developers that
     // this property is private and should not be accessed directly
     // but it is not a full proof way to make a property private
     // it doesn't change anything
+    this.#age = age
+    // this is a new way to make a property private
+    // by using # before the property name
+    // but we need to declare #age outside the constructor
+    // to make it private or else, it will give an error
   }
 
   getBirthYear() {
-    return new Date().getFullYear() - this._age
+    return new Date().getFullYear() - this.#age
   }
   getFullName() {
     return this.firstName + ' ' + this.lastName
@@ -20,3 +27,5 @@ class CreateUser {
 
 const user1 = new CreateUser('Aman', 'Mishra', 32)
 const user2 = new CreateUser('Anurag', 'Singh', 72)
+//user1.#age: 32
+
