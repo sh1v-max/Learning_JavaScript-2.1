2ebcc77c74cc70519a0e597c683e5d81eb5ad352
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-12-09 20:02:10 +0530

    static property

--- 79_Static-property-and-methods-in-JS/script.js
+++ 79_Static-property-and-methods-in-JS/script.js
@@ -20,5 +20,22 @@ class CreateUser {
   }
 }
 
+//~ CreateUser.myName = 'hello world'
+// will be added inside the class as a property
+// console.dir(CreateUser)
+// output:
+  // hi: "Hii"
+  // myName: "hello world"
+  // length: 3
+  // name: "CreateUser"
+  // prototype: {getBirthYear: ƒ, getFullName: ƒ}
+  // arguments: (...)
+  // caller: (...)
+  // [[FunctionLocation]]: script.js:2
+  // [[Prototype]]: ƒ ()
+  // [[Scopes]]: Scopes[2]
+//^ you can not access static property/method on user1, and user2... they are defined for class
+//! will be accessible by only class and not the object
+
 const user1 = new CreateUser('Aman', 'Mishra', 32)
 const user2 = new CreateUser('Anurag', 'Singh', 32)
