4a4ec477b2a414188bfe69ec8632264a2d1d8d58
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-12-10 15:22:46 +0530

    getter and setter

--- 80_Getters-and-Setters-in-JS/script.js
+++ 80_Getters-and-Setters-in-JS/script.js
@@ -1,18 +1,28 @@
-class CreateUser {
-  constructor(firstName, lastName, age) {
-    this.firstName = firstName
-    this.lastName = lastName
-    this.age = age
-  }
+// class CreateUser {
+//   constructor(firstName, lastName, age) {
+//     this.firstName = firstName
+//     this.lastName = lastName
+//     this.age = age
+//   }
 
-  getBirthYear() {
-    return new Date().getFullYear() - this.age
-  }
+//   getBirthYear() {
+//     return new Date().getFullYear() - this.age
+//   }
 
-  getFullName() {
-    return this.firstName + ' ' + this.lastName
+//   getFullName() {
+//     return this.firstName + ' ' + this.lastName
+//   }
+// }
+
+// const user1 = new CreateUser('Aman', 'Mishra', 32)
+// const user2 = new CreateUser('Anurag', 'Singh', 72)
+
+const user = {
+  firstName : 'Anurag',
+  lastName : 'Singh',
+  fullName(){
+    return `${this,this.firstName} ${this.lastName}`
   }
 }
 
-const user1 = new CreateUser('Aman', 'Mishra', 32)
-const user2 = new CreateUser('Anurag', 'Singh', 72)
+console.log(user.fullName())
