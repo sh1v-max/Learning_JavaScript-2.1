beab479fdb242e3a89d0fa43c74c46b85efe28e5
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-12-11 13:21:42 +0530

    class employee

--- 81_prototype-inheritance-using-extends-and-super-keyword-JS/script.js
+++ 81_prototype-inheritance-using-extends-and-super-keyword-JS/script.js
@@ -20,6 +20,9 @@ class User {
   }
 }
 
+// const user1 = new User('Aman', 'Mishra', 32)
+// const user2 = new User('Anurag', 'Singh', 72)
+
 class Student {
   constructor(firstName, lastName, age, standard) {
     this.firstName = firstName
@@ -42,5 +45,28 @@ class Student {
   }
 } 
 
-// const user1 = new User('Aman', 'Mishra', 32)
-// const user2 = new User('Anurag', 'Singh', 72)
+class Employee {
+  constructor(firstName, lastName, age, company) {
+    this.firstName = firstName
+    this.lastName = lastName
+    this.age = age
+    this.company = company
+  }
+  getBirthYear() {
+    return new Date().getFullYear() - this.age
+  }
+
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`
+  }
+
+  set fullName(value) {
+    const [firstName, lastName] = value.split(' ')
+    this.firstName = firstName
+    this.lastName = lastName
+  }
+}
+
+const student1 = new Student('Aman', 'Mishra', 32, 10)
+const student2 = new Student('Aman', 'Mishra', 32, 'BCA')
+const user2 = new User('Anurag', 'Singh', 72)
