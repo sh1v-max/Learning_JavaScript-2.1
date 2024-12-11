43c02beede4b05a787b303015d34e027fd277d03
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-12-11 13:38:01 +0530

    extend in action

--- 81_prototype-inheritance-using-extends-and-super-keyword-JS/script.js
+++ 81_prototype-inheritance-using-extends-and-super-keyword-JS/script.js
@@ -1,3 +1,75 @@
+//~ class User {
+//   constructor(firstName, lastName, age) {
+//     this.firstName = firstName
+//     this.lastName = lastName
+//     this.age = age
+//   }
+
+//   getBirthYear() {
+//     return new Date().getFullYear() - this.age
+//   }
+
+//   get fullName() {
+//     return `${this.firstName} ${this.lastName}`
+//   }
+
+//   set fullName(value) {
+//     const [firstName, lastName] = value.split(' ')
+//     this.firstName = firstName
+//     this.lastName = lastName
+//   }
+// }
+
+// class Student {
+//   constructor(firstName, lastName, age, standard) {
+//     this.firstName = firstName
+//     this.lastName = lastName
+//     this.age = age
+//     this.standard = standard
+//   }
+//   getBirthYear() {
+//     return new Date().getFullYear() - this.age
+//   }
+
+//   get fullName() {
+//     return `${this.firstName} ${this.lastName}`
+//   }
+
+//   set fullName(value) {
+//     const [firstName, lastName] = value.split(' ')
+//     this.firstName = firstName
+//     this.lastName = lastName
+//   }
+// } 
+
+// class Employee {
+//   constructor(firstName, lastName, age, company) {
+//     this.firstName = firstName
+//     this.lastName = lastName
+//     this.age = age
+//     this.company = company
+//   }
+//   getBirthYear() {
+//     return new Date().getFullYear() - this.age
+//   }
+
+//   get fullName() {
+//     return `${this.firstName} ${this.lastName}`
+//   }
+
+//   set fullName(value) {
+//     const [firstName, lastName] = value.split(' ')
+//     this.firstName = firstName
+//     this.lastName = lastName
+//   }
+// }
+
+// const student1 = new Student('Aman', 'Mishra', 32, 10)
+// const employee = new Employee('Aman', 'Mishra', 32, 'google')
+
+//^ in the above code we are repeating the same code again and again for the different classes
+//^ so we can use extends keyword to inherit the properties of the other class
+
 class User {
   constructor(firstName, lastName, age) {
     this.firstName = firstName
@@ -20,53 +92,9 @@ class User {
   }
 }
 
-// const user1 = new User('Aman', 'Mishra', 32)
-// const user2 = new User('Anurag', 'Singh', 72)
+class Student {} 
 
-class Student {
-  constructor(firstName, lastName, age, standard) {
-    this.firstName = firstName
-    this.lastName = lastName
-    this.age = age
-    this.standard = standard
-  }
-  getBirthYear() {
-    return new Date().getFullYear() - this.age
-  }
-
-  get fullName() {
-    return `${this.firstName} ${this.lastName}`
-  }
-
-  set fullName(value) {
-    const [firstName, lastName] = value.split(' ')
-    this.firstName = firstName
-    this.lastName = lastName
-  }
-} 
-
-class Employee {
-  constructor(firstName, lastName, age, company) {
-    this.firstName = firstName
-    this.lastName = lastName
-    this.age = age
-    this.company = company
-  }
-  getBirthYear() {
-    return new Date().getFullYear() - this.age
-  }
-
-  get fullName() {
-    return `${this.firstName} ${this.lastName}`
-  }
-
-  set fullName(value) {
-    const [firstName, lastName] = value.split(' ')
-    this.firstName = firstName
-    this.lastName = lastName
-  }
-}
+class Employee {}
 
 const student1 = new Student('Aman', 'Mishra', 32, 10)
-const student2 = new Student('Aman', 'Mishra', 32, 'BCA')
-const user2 = new User('Anurag', 'Singh', 72)
+const employee = new Employee('Aman', 'Mishra', 32, 'google')
