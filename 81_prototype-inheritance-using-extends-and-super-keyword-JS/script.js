1e7320e3e93d7c39a1fe9c928e3bd73df30aa009
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-12-11 13:14:08 +0530

    extend

--- 81_prototype-inheritance-using-extends-and-super-keyword-JS/script.js
+++ 81_prototype-inheritance-using-extends-and-super-keyword-JS/script.js
@@ -20,5 +20,27 @@ class User {
   }
 }
 
-const user1 = new User('Aman', 'Mishra', 32)
-const user2 = new User('Anurag', 'Singh', 72)
+class Student {
+  constructor(firstName, lastName, age, standard) {
+    this.firstName = firstName
+    this.lastName = lastName
+    this.age = age
+    this.standard = standard
+  }
+  getBirthYear() {
+    return new Date().getFullYear() - this.age
+  }
+
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`
+  }
+
+  set fullName(value) {
+    const [firstName, lastName] = value.split(' ')
+    this.firstName = firstName
+    this.lastName = lastName
+  }
+} 
+
+// const user1 = new User('Aman', 'Mishra', 32)
+// const user2 = new User('Anurag', 'Singh', 72)
