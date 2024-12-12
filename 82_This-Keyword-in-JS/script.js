67b65c0ec99b347f7ece236d599753c23af07835
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-12-12 20:17:38 +0530

    this for function within method inside an object

--- 82_This-Keyword-in-JS/script.js
+++ 82_This-Keyword-in-JS/script.js
@@ -6,6 +6,15 @@
 const user = {
   firstName: 'John',
   lastName : 'Doe',
+  tags: ['a', 'b', 'c'],
+  printTags(){
+    console.log(this.tags)
+    this.tags.forEach(function(tag){
+      console.log(tag)
+      console.log(this.firstName)// this will return undefined because this is pointing to window object
+      // now to let this point to user object we can use "this" after the function
+    }, this) //^ doing this will make "this" inside function point to user object
+  },
   fullname (){
     function getAge(){
       console.log('Getting age')
@@ -16,3 +25,6 @@ const user = {
   }
 }
 user.fullname()
+user.printTags()
+
+//! if there is another function withing a method in an object then "this" will point to window object
