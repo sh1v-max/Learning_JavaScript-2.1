cd76e5361d2d4abe44eafe0ef7e7caab80c3aa26
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-12-14 16:04:00 +0530

    date

--- 83_Dates-in-JS/script.js
+++ 83_Dates-in-JS/script.js
@@ -5,4 +5,11 @@ const myDate = new Date()
 console.log(myDate)
 // console.dir(myDate)
 // in the object formate
-console.log(myDate.getTime()) // 1734163137773
+console.log("time laps since jan 1 1970: " + myDate.getTime()) // 1734163137773
+console.log("date and time: " + myDate.toLocaleString()) // 12/14/2024, 2:36:51 PM // this is american format
+console.log("date and time: " + myDate.toLocaleString('en-GB')) // 14/12/2024, 14:39:01 // this is british/indian format
+// now time has changed into 24 hours format
+console.log("date and time: " + myDate.toLocaleString('en-GB', {hour12: true })) // 14/12/2024, 14:39:01 
+// hour format has changed to 12 hour format
+console.log("date: " + myDate.toLocaleDateString('en-GB', {hour12: true })) // 14/12/2024
+// only date
