bf580ddc50b7a201511a48034c26c1d27ffcb52b
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-12-14 14:34:11 +0530

    date in ms

---
+++ 83_Dates-in-JS/script.js
@@ -0,0 +1,8 @@
+Date()
+// 'Sat Dec 14 2024 13:25:29 GMT+0530 (India Standard Time)' (string)
+const myDate = new Date()
+// Sat Dec 14 2024 13:25:29 GMT+0530 (India Standard Time) (object)
+console.log(myDate)
+// console.dir(myDate)
+// in the object formate
+console.log(myDate.getTime()) // 1734163137773
