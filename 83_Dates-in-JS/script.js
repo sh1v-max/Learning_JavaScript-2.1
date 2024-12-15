37ba4f3852e7ac2f16670aced37addc816e830b1
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-12-15 16:43:52 +0530

    date method

--- 83_Dates-in-JS/script.js
+++ 83_Dates-in-JS/script.js
@@ -1,35 +1,13 @@
-Date()
-// 'Sat Dec 14 2024 13:25:29 GMT+0530 (India Standard Time)' (string)
-const isoDate = '2024-12-14T13:13:44.925Z'
-const myDate = new Date()
-// Sat Dec 14 2024 13:25:29 GMT+0530 (India Standard Time) (object)
-console.log(myDate)
-console.log('ISO format:', myDate.toISOString())
-// console.dir(myDate)
-// in the object formate
-console.log("time laps since jan 1 1970:", myDate.getTime()) // 1734163137773
-console.log("date and time:", myDate.toLocaleString()) // 12/14/2024, 2:36:51 PM // this is american format
-console.log("date and time:", myDate.toLocaleString('en-GB')) // 14/12/2024, 14:39:01 // this is british/indian format
-// now time has changed into 24 hours format
-console.log("date and time:", myDate.toLocaleString('en-GB', {hour12: true })) // 14/12/2024, 14:39:01 
-// hour format has changed to 12 hour format
-console.log("date:", myDate.toLocaleDateString('en-GB')) // 14/12/2024
-// only date
-console.log("day:", myDate.getDate()) // 14
-console.log('month:', myDate.getMonth()+ 1) // 11 
-// month starts from 
-console.log("year:", myDate.getFullYear()) // 2024 
-console.log("UTC year:", myDate.getUTCFullYear()) // 2024 
-console.log("month:", myDate.getMonth()) // 2024 
-console.log("week day:", myDate.getDay()) // 6 // 0 is sunday
-console.log("hours:", myDate.getHours()) // 16
-console.log("minutes:", myDate.getMinutes()) // 16
-console.log("seconds:", myDate.getSeconds())// 45
-console.log("milliseconds:", myDate.getMilliseconds()) // 672 
-console.log("time zone offset:", myDate.getTimezoneOffset()) // -330
-// -ve indicates that local time is ahead of UTCz
-console.log("JSON:", myDate.toJSON()) // -330
+// 
+const myDate = new Date(1734261139001) // passing the timestamp in this will return the date and time 
+// and it will not change further after we console myDate
+console.log(myDate) // it will remain same if there is timestamp passed in the Date object
+// Sun Dec 15 2024 16:42:19 GMT+0530 (India Standard Time)
+console.log(myDate.getTime()) // this will return te time stamp in milliseconds
 
-//^ UTC will show everything but for the UTC time, ie. england time
-console.log("UTC hours:", myDate.getUTCHours()) // 16
-console.log("UTC minutes:", myDate.getUTCMinutes()) // 16
+// have the timestamp of any particular date and time and use that
+// and apply different methods on that to get date or anything you want
+// like below
+console.log(myDate.toLocaleString()) // 12/15/2024, 4:42:19 PM
+console.log(myDate.toLocaleDateString()) //12/15/2024
+console.log(myDate.getMinutes()) // 42
