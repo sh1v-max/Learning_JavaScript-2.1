a64a7db1daf12009f0bdc56addb6b4790a1e18ac
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-12-15 20:34:06 +0530

    date more methods

--- 83_Dates-in-JS/script.js
+++ 83_Dates-in-JS/script.js
@@ -15,3 +15,43 @@ console.log(myDate.toString()) // this will be a string representation of the da
 
 //& simple way to return the current date and time
 console.log(Date.now()) //& this will return the current timestamp in milliseconds 
+
+
+//^ how to see the time before 1970?
+// we can use the negative timestamp to see the time before 1970
+const myDate1 = new Date(-12345678910111) // passing the timestamp in this will return the date and time
+console.log(myDate1) // 1915
+
+//^ how to check based on date of birth or to compare two dates (without time)
+console.log(`===================================`)
+const user1DOB = '02/04/2000'
+const user2DOB = '03/06/1998'
+// MM/DD/YYYY or YYYY/MM/DD Accepted
+// DD-MM-YYYY or DD/MM/YYYY not accepted
+const user3DOB = '2000-04-02T12:00:00.300Z' // passed in ISO string
+// must be in valid ISO format
+// ISO format is: YYYY-MM-DDTHH:MM:SS.MMMZ(or without Z), Z is for UTC time
+const timeStampDate = new Date(23) // passed in timestamp
+// const user2DOB = '03-06-1998'
+const user1Date = new Date(user1DOB.split('/').reverse().join('/')) // to get in the indian format
+const user2Date = new Date('03/06/1998')
+// console.log({user1Date, user2Date})
+console.log(user1Date)
+console.log(user2Date)
+
+//^ compare the two dates to see which user is older
+const user1AgeOnMilliseconds = Date.now() - user1Date.getTime()
+const user2AgeOnMilliseconds = Date.now() - user2Date.getTime()
+console.log(user1AgeOnMilliseconds - user2AgeOnMilliseconds)
+// -65491200000
+// as the result is negative, user2 is older than user1
+
+//^ compare two date of birth (with time)
+const user3Date = new Date('2000-04-02T12:00:00.300Z')
+console.log(user3Date.toLocaleString('en-GB'))
+console.log(user3Date.toLocaleDateString('en-GB'))
+
+const user4DOB = new Date(2000, 11, 12, 4, 30, 15)
+// Tue Dec 12 2000 04:30:15 GMT+0530 (India Standard Time)
+//? https://youtu.be/m5KnQQ-VXmQ?list=PLfEr2kn3s-br9ZFmejfLhAgMbGgbpdof8&t=8475
+console.log(user4DOB)
